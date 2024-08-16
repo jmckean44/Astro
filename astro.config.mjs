3c0c261a6c21b3a567a2bb8022ead14631cedb9c
Jeff McKean <jmckean44@gmail.com>
2024-08-16 12:32:25 -0400

    Seccond attempt to commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,15 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+import node from '@astrojs/node';
+import icon from 'astro-icon';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	site: 'https://riverscape.ca',
+	output: 'static',
+	prefetch: true,
+	integrations: [sitemap(), icon()],
+	adapter: node({
+		mode: 'standalone',
+	}),
+});
