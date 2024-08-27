133efce27f4b30fd850924cb1650aef4abd76c32
Jeff McKean <jmckean44@gmail.com>
2024-08-27 14:23:03 -0400

    outDir: './public_html/staging',

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,9 @@ import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://staging.riverscape.ca',
+	site: 'https://riverscape.ca',
 	output: 'static',
-	//outDir: './public_html',
-	outDir: './staging.riverscape.ca',
+	outDir: './public_html/staging',
 	prefetch: true,
 	integrations: [sitemap(), icon()],
 	adapter: node({
