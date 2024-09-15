0ac1e65bbb89f2bb0dcce73f8d10a192bcbc4484
Jeff McKean <jmckean44@gmail.com>
2024-09-15 11:03:40 -0400

    npx astro add node

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://marinaresortcavtat.com/',
-	output: 'static', // hybrid, server, static = pre-rendered pages
+	output: 'hybrid', // hybrid, server, static = pre-rendered pages
 	outDir: 'staging.marinaresortcavtat.com',
 	prefetch: true,
 	integrations: [sitemap(), icon(), react()],
